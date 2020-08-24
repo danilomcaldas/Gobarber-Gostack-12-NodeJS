c478936e1d02eb180381e604e936efbb2b61c991
Danilo Caldas <magalhaes.danilocaldas@gmail.com>
2020-08-24 12:09:33 -0300

    added validation using lib CELEBRATE

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import ProfileController from '../controllers/ProfileController';
 
@@ -8,7 +9,15 @@ const updateProfileController = new ProfileController();
 profileRouter.use(ensureAuthenticated);
 
 profileRouter.get('/', updateProfileController.show);
-profileRouter.put('/', updateProfileController.update);
+profileRouter.put('/', celebrate({
+    [Segments.BODY]: {
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        old_password: Joi.string(),
+        password: Joi.string(),
+        password_confirmation: Joi.string().valid(Joi.ref('password')),
+    }
+}), updateProfileController.update);
 
 
 export default profileRouter;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
@@ -6,12 +7,19 @@ import UsersController from '@modules/users/infra/http/controllers/UsersControll
 import UserAvatarController from '@modules/users/infra/http/controllers/UserAvatarController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
+
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 const userController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
-usersRouter.post('/', userController.create);
+usersRouter.post('/', celebrate({
+    [Segments.BODY]: {
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+    }
+}), userController.create);
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), userAvatarController.update);
 
